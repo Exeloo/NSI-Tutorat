7cb4630624f5511ff097831c8d7b54bde3e3440d
Exelo <exelo.corp@dmail.com>
2022-04-11 00:19:08 +0200

    Delete unused files

--- src/modules/v-select.ts
+++ src/modules/v-select.ts
@@ -2,6 +2,6 @@ import vSelect from 'vue-select'
 import type { UserModule } from '~/types'
 import 'vue-select/dist/vue-select.css'
 
-export const install: UserModule = ({ app, router, isClient }) => {
+export const install: UserModule = ({ app }) => {
   app.component('VSelect', vSelect)
 }
