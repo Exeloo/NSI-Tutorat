abb426a4af4311ff3eae2ad0547e273e5354829d
Quentin <quentin161204@gmail.com>
2022-05-16 22:50:41 +0200

    index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,33 +1,30 @@
 <template>
   <div class="gray row" >
     <div class="row-content">
-
+        Bienvenue sur le site du <br/>tutorat de François d'Estaing
+        <img class="fde-logo" src="../../public/logo_fde.png">
     </div>
 
   </div>
   <div class="white row" >
     <div class="row-content">
-
+      <img class="book" src="../../public/book.png">
+      Pour apprendre et <br/> progresser ensemble
     </div>
 
   </div>
   <div class="gray row" >
     <div class="row-content">
-
+      Trouver de l'aide <br/> et en donner
+      <img class="book" src="../../public/loupe.png">
     </div>
 
   </div>
   <div class="white row" >
-    <div class="row-content">
-
+    <div class="row-content last">
+      Pret a commencer ?
+      <GoogleAuth />
     </div>
-
-  </div>
-  <div class="gray row" >
-    <div class="row-content">
-
-    </div>
-
   </div>
 </template>
 
@@ -38,6 +35,11 @@
 <style scoped>
 .row {
   width: 100%;
+  display: flex;
+  justify-content: space-around;
+
+  font-size: 5.5vh;
+  padding-top: 10vh;
 }
 
 .gray {
@@ -47,6 +49,16 @@
 .white {
   background-color: var(--main-background);
 }
+
+.row-content{
+  display: flex;
+}
+
+.book {
+ width: 300px;
+}
+last {
+  display: flex;
 
 </style>
 
